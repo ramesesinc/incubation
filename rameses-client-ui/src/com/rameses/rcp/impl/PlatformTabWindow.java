2b9b21a8801063f6ff8441ac952bad27981cf825
worgflores <worgflores@gmail.com>
2013-07-24 14:25:13 +0800

    modified the PlatformTabWindow by implementing the SubWindow

--- rameses-client-ui/src/com/rameses/rcp/impl/PlatformTabWindow.java
+++ rameses-client-ui/src/com/rameses/rcp/impl/PlatformTabWindow.java
@@ -7,11 +7,13 @@
 package com.rameses.rcp.impl;
 
 import com.rameses.platform.interfaces.SubWindow;
+import com.rameses.platform.interfaces.SubWindowListener;
 import com.rameses.platform.interfaces.ViewContext;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Map;
 import javax.swing.JComponent;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
@@ -20,77 +22,91 @@ import javax.swing.KeyStroke;
  *
  * @author jaycverg
  */
-public class PlatformTabWindow extends JPanel implements SubWindow {
-
+public class PlatformTabWindow extends JPanel implements SubWindow 
+{
     private String id;
     private String title;
     private boolean canClose = true;
     private ViewContext viewContext;
     private PlatformImpl platform;
     
-    
     public PlatformTabWindow(String id, Component comp, PlatformImpl platform) {
-        this.id = id;
+        this(id, comp, platform, true);
+    }
+    
+    public PlatformTabWindow(String id, Component comp, PlatformImpl platform, boolean canClose) {
+        this.id = (id == null? "WIN"+new java.rmi.server.UID(): id);
         this.platform = platform;
+        this.canClose = canClose;
         
-        setLayout(new BorderLayout());
-        add(comp);
         registerKeyboardAction(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 close();
             }
         }, KeyStroke.getKeyStroke("ctrl W"), JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+        if (comp instanceof ViewContext) { 
+            viewContext = (ViewContext) comp; 
+            viewContext.setSubWindow(this); 
+        } 
         
-        if( comp instanceof ViewContext ) {
-            viewContext = (ViewContext) comp;
-        }
-    }
-    
-    public PlatformTabWindow(String id, Component comp, PlatformImpl platform, boolean canClose) {
-        this(id, comp, platform);
-        this.canClose = canClose;
+        setLayout(new BorderLayout());        
+        add(comp);         
     }
 
     public void close() {
-        if( !canClose ) return;
-        if( viewContext != null && !viewContext.close() ) return;
+        if ( !canClose ) return;
+        if ( viewContext != null && !viewContext.close() ) return;
         
         Component p = getParent();
-        if( p instanceof ExtTabbedPane ) {
-            ((ExtTabbedPane)p).remove(this);
-        }
-        if( platform != null ) {
+        if (p instanceof ExtTabbedPane) 
+            ((ExtTabbedPane) p).remove(this);                
+        if (platform != null) 
             platform.windows.remove(id);
-        }
     }
 
-    public String getName() { return this.id; } 
-    
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCanClose() {
-        return canClose;
-    }
-
-    public void setCanClose(boolean canClose) {
-        this.canClose = canClose;
-    }
-
-    public void closeWindow() {
-        close();
-    }
+    public boolean isCanClose() { return canClose; }
+    public void setCanClose(boolean canClose) { this.canClose = canClose; }
     
     public void activate() {
         Component p = getParent();
-        if( p instanceof ExtTabbedPane ) {
-            ((ExtTabbedPane)p).setSelectedComponent(this);
-        }
+        if (p instanceof ExtTabbedPane) 
+            ((ExtTabbedPane) p).setSelectedComponent(this);
     }
+    
+    // <editor-fold defaultstate="collapsed" desc=" SubWindow implementation ">
+
+    private SubWindowListener listener;
+    
+    public void setListener(SubWindowListener listener) {
+        this.listener = listener;
+    }
+    
+    public String getName() { return this.id; } 
+    
+    public String getTitle() { return title; }
+    public void setTitle(String title) { 
+        this.title = title; 
+        
+        if (this.listener != null) 
+            this.listener.fireUpdate(this);
+    }
+    
+    public void closeWindow() { close(); }
+
+    public void update(Map attrs) {
+        if (attrs == null || attrs.isEmpty()) return;
+        
+        Object oid = attrs.remove("id");
+        if (oid != null) this.id = oid.toString();
+        
+        Object otitle = attrs.remove("title");
+        if (otitle != null) this.title = otitle.toString();
+        
+        if (this.listener != null) 
+            this.listener.fireUpdate(this); 
+    }
+    
+    // </editor-fold>    
 
 }
