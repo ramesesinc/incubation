58d0976c8e208cf4515bcb75293871bca4cb0ed9
worgflores <worgflores@gmail.com>
2013-07-24 14:16:52 +0800

    added new methods to SubWindow interface *getName() *getTitle() *setListener(SubWindowListener listener) *update(Map windowAttributes)

--- rameses-client-ui/src/com/rameses/platform/interfaces/SubWindow.java
+++ rameses-client-ui/src/com/rameses/platform/interfaces/SubWindow.java
@@ -1,10 +1,19 @@
 package com.rameses.platform.interfaces;
 
+import java.util.Map;
+
 public interface SubWindow 
 {    
     static final long serialVersionUID = 1L;
     
-    void closeWindow();
+    String getName();
+    
+    String getTitle();
     void setTitle(String title);
     
+    void closeWindow(); 
+        
+    void setListener(SubWindowListener listener);
+    
+    void update(Map windowAttributes);
 }
