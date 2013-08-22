d30e0beddc062ea432f6a82a8d3c682650baaa4a
worgflores <worgflores@gmail.com>
2013-08-22 12:25:54 +0800

    updated the ActionProvider by adding a method lookupOpener

--- rameses-client-ui/src/com/rameses/rcp/framework/ActionProvider.java
+++ rameses-client-ui/src/com/rameses/rcp/framework/ActionProvider.java
@@ -1,7 +1,9 @@
 package com.rameses.rcp.framework;
 
 import com.rameses.rcp.common.Action;
+import com.rameses.rcp.common.Opener;
 import java.util.List;
+import java.util.Map;
 
 /**
  * This interface will be implemented to provide actions
@@ -14,4 +16,5 @@ public interface ActionProvider {
     List<Action> getActions(String category, Object context);
     List<Action> getActionsByType(String type, UIController controller);
     List<Action> lookupActions(String actionType); 
+    Opener lookupOpener(String actionType, Map params);
 }
