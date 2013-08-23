6f0622aa7493fc694b7a0156b918eec797e7645b
worgflores <worgflores@gmail.com>
2013-08-23 10:35:05 +0800

    updated the OsirisSecurityProvider

--- osiris2-client-ui/src/com/rameses/osiris2/client/OsirisSecurityProvider.java
+++ osiris2-client-ui/src/com/rameses/osiris2/client/OsirisSecurityProvider.java
@@ -21,23 +21,37 @@ public class OsirisSecurityProvider implements SecurityProvider, ClientSecurityP
 
     public boolean checkPermission(String domain, String role, String name) {
         Map roles = (Map) OsirisContext.getEnv().get("ROLES");
-        if(roles!=null && role!=null ) {
-            if(domain!=null) role = domain+"."+role;
-            if(! roles.containsKey(role)) return false;
-            if(name==null || name.trim().length()==0) return true;
-            String disallowed = (String)roles.get(role);
-            if(disallowed!=null && name.matches(disallowed))return false; 
-            return true;
+        if (roles != null && role != null && role.length() > 0) {
+            String[] arrays = role.split(",");
+            for (int i=0; i<arrays.length; i++) {
+                String srole = arrays[i].trim();
+                if (domain != null) srole = domain+"."+srole;
+                if (!roles.containsKey(srole)) continue;
+                
+                if (name == null || name.trim().length() == 0) return true;
+                
+                String disallowed = (String) roles.get(srole);
+                if (disallowed != null && name.matches(disallowed)) continue;
+                
+                return true;
+            } 
+            return false; 
+//            if(domain!=null) role = domain+"."+role;
+//            if(! roles.containsKey(role)) return false;
+//            if(name==null || name.trim().length()==0) return true;
+//            String disallowed = (String)roles.get(role);
+//            if(disallowed!=null && name.matches(disallowed))return false; 
+//            return true;
         } 
-        if(roles!=null) {
+        
+        if (roles != null) { 
             String allowed = (String)roles.get("ALLOWED");
-            if(allowed!=null && name.matches(allowed)) return true;
-            return false;
+            if (allowed != null && name.matches(allowed)) 
+                return true; 
+            else 
+                return false;
         }
         
-        if (name == null) return true;
-        return false;
+        return (name == null? true: false);
     }
-
-    
 }
