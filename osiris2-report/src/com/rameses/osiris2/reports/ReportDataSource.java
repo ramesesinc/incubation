b2ad87e21d559cc9b97fe08dbe450aedd80ef7ee
worgflores <worgflores@gmail.com>
2013-08-23 20:33:37 +0800

    added osiris2-report

---
+++ osiris2-report/src/com/rameses/osiris2/reports/ReportDataSource.java
@@ -0,0 +1,73 @@
+
+package com.rameses.osiris2.reports;
+
+import com.rameses.common.PropertyResolver;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRField;
+
+public class ReportDataSource implements JRDataSource 
+{
+    protected Iterator iterator;
+    private Object source;
+    protected Object currentObject;
+    private PropertyResolver propertyResolver;
+
+    public ReportDataSource(Object source) {
+        setSource(source);
+        propertyResolver = PropertyResolver.getInstance();
+    }
+    
+    public void setSource(Object src) {
+        this.source = src;
+        if( src == null ) {
+            iterator = (new ArrayList()).iterator();
+        }
+        else if( src instanceof Collection ) {
+            iterator = ((Collection)src).iterator();
+        }
+        else {
+            List l = new ArrayList();
+            l.add( src );
+            iterator = l.iterator();
+        }
+    }
+
+    public boolean next() throws JRException {
+        if( iterator.hasNext() ) {
+            currentObject = iterator.next();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    
+    
+    public Object getFieldValue(JRField jRField) throws JRException {
+        Object field = null;
+        String fieldName = null;
+        try {
+            fieldName = jRField.getName();
+            //field = PropertyUtils.getNestedProperty( currentObject, fieldName );
+            field = propertyResolver.getProperty(currentObject, fieldName);
+            
+            if( jRField.getValueClass().isAssignableFrom( Collection.class) ) {
+                return new ReportDataSource( field );
+            }
+            else {
+                return field;
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println("Error on field [" + fieldName  + "] caused by " + ex.getMessage());
+            return null;
+        }
+    }    
+}
---
+++ osiris2-report/src/com/rameses/osiris2/reports/ReportModel.java
@@ -0,0 +1,106 @@
+/*
+ * ReportData.java
+ *
+ * Created on November 25, 2009, 2:25 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.osiris2.reports;
+
+import com.rameses.rcp.common.Action;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import net.sf.jasperreports.engine.JRDataSource;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+
+/**
+ *
+ * @author elmo
+ */
+public abstract class ReportModel {
+    
+    private boolean allowSave = false;
+    private boolean allowPrint = true;
+        
+    public abstract Object getReportData();
+    public abstract String getReportName();
+    
+    public boolean isAllowSave() { return allowSave; } 
+    public void setAllowSave(boolean allowSave) { 
+        this.allowSave = allowSave; 
+    }
+    
+    public boolean isAllowPrint() { return allowPrint; } 
+    public void setAllowPrint(boolean allowPrint) {
+        this.allowPrint = allowPrint;
+    }
+    
+    public SubReport[] getSubReports() { return null; }    
+    
+    public Map getParameters() { return null; }
+    
+    private JasperPrint reportOutput;
+    
+    private JasperReport mainReport;
+    
+    private JasperPrint createReport() {
+        try {
+            if (mainReport == null) {
+                mainReport = ReportUtil.getJasperReport(getReportName());
+            }
+            
+            Map conf = new HashMap();
+            SubReport[] subReports = getSubReports();
+            
+            if (subReports != null) {
+                for (SubReport sr: subReports) {
+                    conf.put( sr.getName(), sr.getReport() );
+                }
+            }
+            Map params = getParameters();
+            if (params != null) conf.putAll(params);
+            
+            JRDataSource ds = null;
+            Object data = getReportData();
+            if (data != null) {
+                ds = new ReportDataSource(data);
+            } else {
+                ds = new JREmptyDataSource();
+            }
+            return JasperFillManager.fillReport(mainReport, conf, ds);
+        } 
+        catch (RuntimeException re) {
+            throw re; 
+        } catch (JRException ex) {
+            ex.printStackTrace();
+            throw new IllegalStateException(ex.getMessage(), ex);
+        }
+    }
+    
+    public String viewReport() {
+        reportOutput = createReport();
+        return "report";
+    }
+    
+    public JasperPrint getReport() { 
+        return reportOutput;
+    }
+        
+    public List getReportActions() {
+        List list = new ArrayList();
+        list.add( new Action("_close", "Close", null));
+        return list;
+    } 
+    
+    //this method is invoked by the back button
+    public Object back() { return "_close"; } 
+    
+}
