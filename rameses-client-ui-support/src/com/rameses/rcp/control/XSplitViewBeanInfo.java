151c835be35e0deceba2e29aa01a9eb77ae6a414
worgflores <worgflores@gmail.com>
2013-08-26 10:45:13 +0800

    added class XSplitViewBeanInfo

---
+++ rameses-client-ui-support/src/com/rameses/rcp/control/XSplitViewBeanInfo.java
@@ -0,0 +1,39 @@
+/*
+ * XSplitViewBeanInfo.java
+ *
+ * Created on August 25, 2013, 9:44 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.rcp.control;
+
+import com.rameses.beaninfo.ComponentBeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+/**
+ *
+ * @author wflores
+ */
+public class XSplitViewBeanInfo extends ComponentBeanInfo.Support
+{
+    private Class beanClass;
+    
+    protected Class getBeanClass() {
+        if (beanClass == null) beanClass = XSplitView.class;
+        
+        return beanClass; 
+    }
+    
+    
+    protected PropertyDescriptor[] createPropertyDescriptors() throws IntrospectionException 
+    {
+        return new PropertyDescriptor[] {
+            new PropertyDescriptor("border", getBeanClass()),
+            new PropertyDescriptor("dividerLocation", getBeanClass()),
+            new PropertyDescriptor("dividerSize", getBeanClass())
+        }; 
+    }
+}
