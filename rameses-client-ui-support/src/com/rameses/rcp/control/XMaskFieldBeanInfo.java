35420318bca7d4bb689f8dbb6309270bdf92e161
worgflores <worgflores@gmail.com>
2013-09-02 15:05:08 +0800

    added new class XMaskFieldBeanInfo

---
+++ rameses-client-ui-support/src/com/rameses/rcp/control/XMaskFieldBeanInfo.java
@@ -0,0 +1,57 @@
+/*
+ * XMaskFieldBeanInfo.java
+ *
+ * Created on September 2, 2013, 1:34 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.rcp.control;
+
+import com.rameses.beaninfo.ComponentBeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+/**
+ *
+ * @author wflores
+ */
+public class XMaskFieldBeanInfo extends ComponentBeanInfo.Support 
+{
+    private Class beanClass;
+    
+    protected Class getBeanClass() 
+    {
+        if (beanClass == null) beanClass = XMaskField.class; 
+        
+        return beanClass;
+    }
+    
+    protected PropertyDescriptor[] createPropertyDescriptors() throws IntrospectionException 
+    {
+        return new PropertyDescriptor[] {
+            new PropertyDescriptor("text", getBeanClass()),
+            new PropertyDescriptor("editable", getBeanClass(), "isEditable", "setEditable"),
+            new PropertyDescriptor("border", getBeanClass()), 
+            new PropertyDescriptor("margin", getBeanClass()), 
+            new PropertyDescriptor("fontStyle", getBeanClass()), 
+            
+            new PropertyDescriptor("caption", getBeanClass()),
+            new PropertyDescriptor("captionFont", getBeanClass()),
+            new PropertyDescriptor("captionFontStyle", getBeanClass()),
+            new PropertyDescriptor("captionMnemonic", getBeanClass()),
+            new PropertyDescriptor("captionWidth", getBeanClass()),
+            new PropertyDescriptor("showCaption", getBeanClass(), "isShowCaption", "setShowCaption"),            
+            new PropertyDescriptor("cellPadding", getBeanClass()),
+            
+            new PropertyDescriptor("depends", getBeanClass()),
+            new PropertyDescriptor("index", getBeanClass()),
+            new PropertyDescriptor("includeLiteral", getBeanClass()),
+            new PropertyDescriptor("mask", getBeanClass()),
+            
+            new PropertyDescriptor("readonly", getBeanClass(), "isReadonly", "setReadonly"),
+            new PropertyDescriptor("required", getBeanClass(), "isRequired", "setRequired")
+        }; 
+    }
+}
