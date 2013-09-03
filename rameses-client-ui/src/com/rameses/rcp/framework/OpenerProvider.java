49d7bbf99772c16635ee4978baa805383cd5b3a3
worgflores <worgflores@gmail.com>
2013-09-03 10:28:24 +0800

    updated the OpenerProvider

--- rameses-client-ui/src/com/rameses/rcp/framework/OpenerProvider.java
+++ rameses-client-ui/src/com/rameses/rcp/framework/OpenerProvider.java
@@ -9,10 +9,13 @@ package com.rameses.rcp.framework;
 
 import com.rameses.rcp.common.Opener;
 import java.util.List;
-
+import java.util.Map;
 
 public interface OpenerProvider {
     
+    @Deprecated
     List<Opener> getOpeners(String type, Object context);
     
+    Opener lookupOpener(String invokerType, Map params); 
+    List<Opener> lookupOpeners(String invokerType, Map params);
 }
