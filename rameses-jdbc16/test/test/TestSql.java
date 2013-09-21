b5df7e95fc7ca897309f215b5c9c396a8a1a0979
elmo <elmonazareno@gmail.com>
2013-09-22 06:33:22 +0800

    edited soiris 2 report and rameses jdbc

--- rameses-jdbc16/test/test/TestSql.java
+++ rameses-jdbc16/test/test/TestSql.java
@@ -33,11 +33,13 @@ public class TestSql extends TestCase {
     // TODO add test methods here. The name must begin with 'test'. For example:
     public void testHello() throws Exception {
         Class.forName(DBServiceDriver.class.getName()); 
-        Connection c = DriverManager.getConnection( "jdbc:rameses://192.168.254.24:8080/surigao" );
+        Connection c = DriverManager.getConnection( "jdbc:rameses://localhost:8070/osiris3/etracs221" );
         System.out.println(c);
         Statement s = c.createStatement();
-        ResultSet rs = s.executeQuery("select * from af");
-
+        ResultSet rs = s.executeQuery("select * from sariaya_etracs.account limit 5");
+        while(rs.next()) {
+            System.out.println(rs.getString("accttitle"));
+        }
         rs.close();
         s.close();
         c.close();
