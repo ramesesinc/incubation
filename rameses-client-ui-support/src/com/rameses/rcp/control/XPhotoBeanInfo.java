883e738d2f0aaaae7a3eb9e0a66525a9165e0901
worgflores <worgflores@gmail.com>
2013-12-05 14:56:19 +0800

    added XPhotoBeanInfo

---
+++ rameses-client-ui-support/src/com/rameses/rcp/control/XPhotoBeanInfo.java
@@ -0,0 +1,41 @@
+/*
+ * XPhotoBeanInfo.java
+ *
+ * Created on December 4, 2013, 11:10 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.rcp.control;
+
+import com.rameses.beaninfo.ComponentBeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+/**
+ *
+ * @author wflores
+ */
+public class XPhotoBeanInfo extends ComponentBeanInfo.Support
+{
+    private Class beanClass;
+    
+    protected Class getBeanClass() {
+        if (beanClass == null) beanClass = XPhoto.class;
+        
+        return beanClass; 
+    }
+    
+    
+    protected PropertyDescriptor[] createPropertyDescriptors() throws IntrospectionException 
+    {
+        return new PropertyDescriptor[] {
+            new PropertyDescriptor("border", getBeanClass()),
+            new PropertyDescriptor("text", getBeanClass()), 
+            
+            new PropertyDescriptor("depends", getBeanClass()),
+            new PropertyDescriptor("index", getBeanClass()) 
+        };        
+    }
+}
