4d1f0341d3ecd2f25a421556294c4dc1c690f971
worgflores <worgflores@gmail.com>
2013-12-07 15:24:46 +0800

    added XActionFieldBeanInfo

---
+++ rameses-client-ui-support/src/com/rameses/rcp/control/XActionFieldBeanInfo.java
@@ -0,0 +1,60 @@
+/*
+ * XActionFieldBeanInfo.java
+ *
+ * Created on December 7, 2013, 10:43 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.rcp.control;
+
+import com.rameses.beaninfo.ComponentBeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+
+/**
+ *
+ * @author wflores
+ */
+public class XActionFieldBeanInfo extends ComponentBeanInfo.Support 
+{
+    private Class beanClass;
+    
+    protected Class getBeanClass() 
+    {
+        if (beanClass == null) beanClass = XActionField.class; 
+        
+        return beanClass;
+    }
+    
+    protected PropertyDescriptor[] createPropertyDescriptors() throws IntrospectionException 
+    {
+        return new PropertyDescriptor[] {
+            new PropertyDescriptor("border", getBeanClass()),
+            new PropertyDescriptor("expression", getBeanClass()), 
+            new PropertyDescriptor("fontStyle", getBeanClass()), 
+            new PropertyDescriptor("handler", getBeanClass()), 
+            new PropertyDescriptor("spacing", getBeanClass()), 
+            new PropertyDescriptor("readonly", getBeanClass(), "isReadonly", "setReadonly"), 
+            
+            new PropertyDescriptor("actionFont", getBeanClass()), 
+            new PropertyDescriptor("actionFontStyle", getBeanClass()), 
+            new PropertyDescriptor("actionIcon", getBeanClass()), 
+            new PropertyDescriptor("actionText", getBeanClass()), 
+            new PropertyDescriptor("actionTextMargin", getBeanClass()), 
+            
+            new PropertyDescriptor("caption", getBeanClass()),
+            new PropertyDescriptor("captionFont", getBeanClass()),
+            new PropertyDescriptor("captionFontStyle", getBeanClass()),
+            new PropertyDescriptor("captionMnemonic", getBeanClass()),
+            new PropertyDescriptor("captionWidth", getBeanClass()),
+            new PropertyDescriptor("showCaption", getBeanClass(), "isShowCaption", "setShowCaption"),            
+            new PropertyDescriptor("cellPadding", getBeanClass()),
+            
+            new PropertyDescriptor("depends", getBeanClass()),
+            new PropertyDescriptor("index", getBeanClass()),
+            new PropertyDescriptor("readonly", getBeanClass(), "isReadonly", "setReadonly") 
+        }; 
+    }
+}
