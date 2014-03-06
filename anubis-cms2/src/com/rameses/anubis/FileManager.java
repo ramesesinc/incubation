76d8bdf272a25cbbcd221ce6f861bdcadecf86ee
worgflores <worgflores@gmail.com>
2014-03-06 13:31:29 +0800

    updated the FileManager

--- anubis-cms2/src/com/rameses/anubis/FileManager.java
+++ anubis-cms2/src/com/rameses/anubis/FileManager.java
@@ -53,7 +53,7 @@ public class FileManager {
                 return ContentUtil.getResources(urls,name);
             }
         } catch(Exception e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(e.getMessage(), e);
         }
         //locate all sources until you find the file source
         
