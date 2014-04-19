3e47cd2d4227686806e8ae25c38e568162e5bd97
worgflores <worgflores@gmail.com>
2014-04-19 17:31:44 +0800

    updated the TextCase

--- rameses-client-ui/src/com/rameses/rcp/constant/TextCase.java
+++ rameses-client-ui/src/com/rameses/rcp/constant/TextCase.java
@@ -15,8 +15,34 @@ public enum TextCase
     public String convert(String value)
     {
         if (value == null) return value;
-        else if (type == 1) return value.toLowerCase();
-        else if (type == 2) return value.toUpperCase();
+        else if (type == 1) return toLower(value);
+        else if (type == 2) return toUpper(value); 
         else return value;
     }
+    
+    private String toUpper(String value) {
+        StringBuffer sb = new StringBuffer(value); 
+        for (int i=0; i<sb.length(); i++) {
+            char ch = sb.charAt(i);
+            int num = (int)ch; 
+            if ((num >= 65 && num <= 90)  || (num >= 97 && num <= 122) || 
+                (num >= 225 && num <= 255) || (num >= 193 && num <= 376)) {
+                sb.replace(i, i+1, Character.toUpperCase(ch)+""); 
+            } 
+        }
+        return sb.toString(); 
+    }
+    
+    private String toLower(String value) {
+        StringBuffer sb = new StringBuffer(value); 
+        for (int i=0; i<sb.length(); i++) {
+            char ch = sb.charAt(i);
+            int num = (int)ch; 
+            if ((num >= 65 && num <= 90)  || (num >= 97 && num <= 122) || 
+                (num >= 225 && num <= 255) || (num >= 193 && num <= 376)) {
+                sb.replace(i, i+1, Character.toLowerCase(ch)+""); 
+            } 
+        }
+        return sb.toString(); 
+    }    
 }
