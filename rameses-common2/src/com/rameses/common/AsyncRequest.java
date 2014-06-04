7ca1c69443ebc006fcd30ef555f8457c2d0521fc
worgflores <worgflores@gmail.com>
2014-06-04 11:37:59 +0800

    updated the AsyncRequest

--- rameses-common2/src/com/rameses/common/AsyncRequest.java
+++ rameses-common2/src/com/rameses/common/AsyncRequest.java
@@ -38,62 +38,38 @@ public class AsyncRequest implements Serializable {
         return id;
     }
 
-    public String getConnection() {
-        return connection;
-    }
-
+    public String getConnection() { return connection; }
     public void setConnection(String connection) {
         this.connection = connection;
     }
 
-    public String getMethodName() {
-        return methodName;
-    }
-
+    public String getMethodName() { return methodName; }
     public void setMethodName(String methodName) {
         this.methodName = methodName;
     }
 
-    public String getServiceName() {
-        return serviceName;
-    }
-
+    public String getServiceName() { return serviceName; }
     public void setServiceName(String serviceName) {
         this.serviceName = serviceName;
     }
 
-    public Object[] getArgs() {
-        return args;
-    }
-
+    public Object[] getArgs() { return args; }
     public void setArgs(Object[] args) {
         this.args = args;
     }
 
-    public Map getEnv() {
-        return env;
-    }
-
+    public Map getEnv() { return env; }
     public void setEnv(Map env) {
         this.env = env;
     }
 
-    public String getContextName() {
-        return contextName;
-    }
-
+    public String getContextName() { return contextName; }
     public void setContextName(String contextName) {
         this.contextName = contextName;
     }
 
-    public String getVarStatus() {
-        return varStatus;
-    }
-
+    public String getVarStatus() { return varStatus; }
     public void setVarStatus(String varStatus) {
         this.varStatus = varStatus;
     }
-    
-    
-    
 }
