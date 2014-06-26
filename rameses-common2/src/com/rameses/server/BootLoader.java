46c10d00aac605bf85a863fa6a7acdd5e760767d
worgflores <worgflores@gmail.com>
2014-06-26 11:50:57 +0800

    updated the BootLoader

--- rameses-common2/src/com/rameses/server/BootLoader.java
+++ rameses-common2/src/com/rameses/server/BootLoader.java
@@ -5,6 +5,7 @@ import com.rameses.util.Service;
 import java.io.File;
 import java.util.Hashtable;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -51,7 +52,7 @@ public final class BootLoader
         while (entries.hasNext()) {
             Map.Entry entry = (Map.Entry) entries.next();
             String groupName = entry.getKey().toString();
-            Map groupConf = (Map)entry.getValue();
+            Map groupConf = resolveConf((Map) entry.getValue());
             String providerName = (String) groupConf.get("provider");
             
             if(providerName !=null) {
@@ -114,7 +115,57 @@ public final class BootLoader
         //remove the file
         try { file.delete(); } catch(Throwable t) {;}
         return true; 
+    } 
+    
+    private Map resolveConf(Map conf) {
+        Map newconf = new LinkedHashMap();
+        if (conf == null) return newconf;
+        
+        Iterator keys = conf.keySet().iterator(); 
+        while (keys.hasNext()) {
+            Object key = keys.next(); 
+            Object val = resolveValue(conf.get(key), newconf); 
+            newconf.put(key, val); 
+        } 
+        return newconf;
     }
+    
+    private Object resolveValue(Object value, Map conf) { 
+        if (value == null) return null;
+                 
+        int startidx = 0; 
+        boolean has_expression = false; 
+        String str = value.toString();         
+        StringBuilder builder = new StringBuilder(); 
+        while (true) {
+            int idx0 = str.indexOf("${", startidx);
+            if (idx0 < 0) break;
+            
+            int idx1 = str.indexOf("}", idx0); 
+            if (idx1 < 0) break;
+            
+            has_expression = true; 
+            String skey = str.substring(idx0+2, idx1); 
+            builder.append(str.substring(startidx, idx0)); 
+            
+            Object objval = conf.get(skey); 
+            if (objval == null) objval = System.getProperty(skey); 
+            
+            if (objval == null) { 
+                builder.append(str.substring(idx0, idx1+1)); 
+            } else { 
+                builder.append(objval); 
+            } 
+            startidx = idx1+1; 
+        } 
+        
+        if (has_expression) {
+            builder.append(str.substring(startidx));  
+            return builder.toString(); 
+        } else {
+            return value; 
+        }
+    }     
     
     private class ShutdownAgent implements Runnable 
     {
