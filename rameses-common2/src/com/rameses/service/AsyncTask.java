eea91a91ccc5a592690007d986d7fcb86db8f07b
worgflores <worgflores@gmail.com>
2014-10-07 06:13:27 +0800

    updated the rameses-common2: - AbstractAsyncHandler - AsyncTask

--- rameses-common2/src/com/rameses/service/AsyncTask.java
+++ rameses-common2/src/com/rameses/service/AsyncTask.java
@@ -1,17 +1,18 @@
 package com.rameses.service;
 
+import com.rameses.common.AbstractAsyncHandler;
 import com.rameses.common.AsyncBatchResult;
 import com.rameses.common.AsyncException;
 import com.rameses.common.AsyncHandler;
 import com.rameses.common.AsyncToken;
 import java.util.concurrent.TimeoutException;
 
-public class AsyncTask implements Runnable {
-    
+public class AsyncTask implements Runnable 
+{
+    private ServiceProxy proxy;
     private String methodName;
     private Object[] args;
-    private AsyncHandler handler;
-    private ServiceProxy proxy;
+    private AsyncHandlerProxy handlerProxy;
     
     private AsyncPoller poller; 
     private int retrycount;
@@ -20,20 +21,8 @@ public class AsyncTask implements Runnable {
         this.proxy = proxy;
         this.methodName = methodName;
         this.args = args;
-        this.handler = handler;
-        if (this.handler == null) {
-            this.handler = new AsyncHandler() {
-                public void onMessage(Object value) {
-                    System.out.println("unhandled message. No handler passed ");
-                }
-                public void onError(Exception e) {
-                    e.printStackTrace();
-                }
-                public void call(Object o) {
-                    
-                }
-            };
-        }
+        
+        handlerProxy = new AsyncHandlerProxy(handler); 
     }
     
     public void run() {
@@ -44,7 +33,7 @@ public class AsyncTask implements Runnable {
             if (result instanceof AsyncToken) {
                 AsyncToken token = (AsyncToken)result; 
                 if (token.isClosed()) {
-                    handler.onMessage(AsyncHandler.EOF); 
+                    handlerProxy.onMessage(AsyncHandler.EOF); 
                     return;
                 }
                 
@@ -56,16 +45,26 @@ public class AsyncTask implements Runnable {
             
         }  catch (Exception e) {
             e.printStackTrace();
-            handler.onError( e );
+            handlerProxy.onError( e );
         }
     } 
     
     private void handle(AsyncPoller poller, Object result) throws Exception {
+        if (handlerProxy.isCancelRequested()) {
+            try {
+                handlerProxy.onCancel(); 
+            } catch(Throwable t) {
+                t.printStackTrace(); 
+            } finally {
+                return; 
+            }
+        }
+        
         if (result instanceof AsyncToken) {
             AsyncToken at = (AsyncToken)result;
             if (at.isClosed()) {
                 poller.close(); 
-                handler.onMessage(AsyncHandler.EOF); 
+                handlerProxy.onMessage(AsyncHandler.EOF); 
                 return;
             } 
         } 
@@ -75,21 +74,25 @@ public class AsyncTask implements Runnable {
             if (retrycount < 3) {
                 handle(poller, poller.poll()); 
             } else {
-                handler.onError(new TimeoutException("poll failed after 3 retries")); 
+                handlerProxy.onTimeout("poll failed after 3 retries");
+                if (handlerProxy.isRetryRequested()) {
+                    retrycount = 0;
+                    handle(poller, poller.poll()); 
+                }
             }
         } else { 
             if (notify( result )) { 
                 handle(poller, poller.poll()); 
             } else { 
                 poller.close(); 
-                handler.onMessage(AsyncHandler.EOF); 
+                handlerProxy.onMessage(AsyncHandler.EOF); 
             } 
         } 
     } 
     
     private boolean notify(Object o) {
         if (o instanceof AsyncException) {
-            handler.onError((AsyncException) o);
+            handlerProxy.onError((AsyncException) o);
             return false;
             
         } else if (o instanceof AsyncBatchResult) {
@@ -100,18 +103,86 @@ public class AsyncTask implements Runnable {
                     is_closed = ((AsyncToken)item).isClosed(); 
                     
                 } else if (item instanceof AsyncException) {
-                    handler.onError((AsyncException) item);
+                    handlerProxy.onError((AsyncException) item);
                     return false; 
                     
                 } else {
-                    handler.onMessage(item); 
+                    handlerProxy.onMessage(item); 
                 } 
             } 
             return !is_closed; 
             
         } else {
-            handler.onMessage(o); 
+            handlerProxy.onMessage(o); 
             return true; 
         } 
+    }
+        
+    private class AsyncHandlerProxy extends AbstractAsyncHandler {
+        private AsyncHandler source;
+        
+        AsyncHandlerProxy(AsyncHandler source) {
+            this.source = source; 
+            if (source == null) {
+                this.source = new AsyncHandler() {
+                    public void onMessage(Object value) {
+                        System.out.println("unhandled message. No handler passed ");
+                    }
+                    public void onError(Exception e) {
+                        e.printStackTrace();
+                    }
+                    public void call(Object o) {
+                    }
+                };
+            }
+        }
+
+        public void onError(Exception e) {
+            if (source != null) {
+                source.onError(e);
+            } 
+        }
+
+        public void onMessage(Object o) {
+            if (source != null) {
+                source.onMessage(o);
+            } 
+        }
+
+        public void call(Object o) {
+            if (source != null) {
+                source.call(o);
+            } 
+        }
+
+        public void onTimeout(String msg) {
+            if (source instanceof AbstractAsyncHandler) {
+                ((AbstractAsyncHandler) source).onTimeout(); 
+            } else if (source != null) {
+                source.onError(new TimeoutException(msg)); 
+            }
+        }
+        
+        public void onCancel() {
+            if (source instanceof AbstractAsyncHandler) {
+                ((AbstractAsyncHandler) source).onCancel(); 
+            } 
+        }
+        
+        public boolean isRetryRequested() {
+            if (source instanceof AbstractAsyncHandler) {
+                return ((AbstractAsyncHandler) source).hasRequestRetry();
+            } else {
+                return false; 
+            }
+        }
+        
+        public boolean isCancelRequested() {
+            if (source instanceof AbstractAsyncHandler) {
+                return ((AbstractAsyncHandler) source).hasRequestCancel();
+            } else {
+                return false; 
+            }
+        }        
     }
 }
