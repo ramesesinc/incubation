0965f6dfeb3139b1e2ff3960e6ebc96903cab38f
worgflores <worgflores@gmail.com>
2015-01-15 21:05:31 +0800

    updated the rameses-common2

--- rameses-common2/src/com/rameses/util/NumberUtil.java
+++ rameses-common2/src/com/rameses/util/NumberUtil.java
@@ -17,11 +17,13 @@ public final class NumberUtil {
     private static DecimalFormat defaultFormat = new DecimalFormat("###0.00");
     
     public static BigDecimal formatDecimal(Number num, String pattern) {
+        if( num == null ) return new BigDecimal("0");
         DecimalFormat df = new DecimalFormat(pattern);
         return new BigDecimal( df.format(num)  );
     }
     
     public static BigDecimal round(Number num) {
+        if( num == null ) return new BigDecimal("0");
         return new BigDecimal(  defaultFormat.format(num) );
     }
     
