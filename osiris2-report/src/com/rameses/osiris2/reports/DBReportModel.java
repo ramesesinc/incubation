f3097b0d311a67ecd11a0eee8bfa65083b44b8cf
worgflores <worgflores@gmail.com>
2015-08-12 07:23:24 -0700

    updated the DBReportModel

--- osiris2-report/src/com/rameses/osiris2/reports/DBReportModel.java
+++ osiris2-report/src/com/rameses/osiris2/reports/DBReportModel.java
@@ -10,12 +10,10 @@
 package com.rameses.osiris2.reports;
 
 import com.rameses.osiris2.AppContext;
-import com.rameses.rcp.common.Action;
 import com.rameses.service.jdbc.DBServiceDriver;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -29,31 +27,66 @@ import net.sf.jasperreports.engine.JasperReport;
  *
  * @author Elmo
  */
-public abstract class DBReportModel {
+public abstract class DBReportModel  {
     
-    private boolean allowSave = false;
-    private boolean allowPrint = true;
-    private String mode = "initial";
-    
+    private Map params = new HashMap();
+    private String mode = "init"; 
+    private boolean dynamic; 
+
+    private ReportModelImpl report = new ReportModelImpl();
+    private FormPanelModelImpl fpModel = new FormPanelModelImpl();
+            
     public abstract String getReportName();
     
-    public boolean isAllowSave() { return allowSave; }
-    public void setAllowSave(boolean allowSave) {
-        this.allowSave = allowSave;
+    public boolean isDynamic() { return dynamic; } 
+    public void setDynamic(boolean dynamic) {
+        this.dynamic = dynamic; 
     }
     
-    public boolean isAllowPrint() { return allowPrint; }
-    public void setAllowPrint(boolean allowPrint) {
-        this.allowPrint = allowPrint;
+    public String getMode() { return mode; }
+    public void setMode( String mode ) { 
+        this.mode = mode;  
     }
     
-    public SubReport[] getSubReports() { return null; }
+    public SubReport[] getSubReports() {
+        return null; 
+    }
+    public Map getQuery() {
+        return params; 
+    }
+
     
-    public Map getParameters() { return null; }
+    public String getContext() {
+        return (String) AppContext.getInstance().getEnv().get("app.context");
+    }
+    public String getCluster() {
+        return (String) AppContext.getInstance().getEnv().get("app.cluster");
+    }
     
-    private JasperPrint reportOutput;
+    public String back() {
+        setMode( "init" ); 
+        return "default"; 
+    }
     
-    private JasperReport mainReport;
+    
+    public String viewReport() {
+        report.createReport(); 
+        return "report";
+    }
+    
+    public String preview() {
+        String outcome = viewReport(); 
+        setMode( "view" ); 
+        return outcome; 
+    }
+    
+    public Object getReport() {
+        return report;
+    }       
+    
+    public List getFormControls() { 
+        return null; 
+    }  
     
     public class CustomReportClassLoader extends ClassLoader {
         private String parentName;
@@ -66,84 +99,90 @@ public abstract class DBReportModel {
         public URL getResource(String name) {
             return getClass().getClassLoader().getResource( parentName +  name );
         }
+    } 
+    
+    public Object getFormControl(){
+        return fpModel;
     }
     
+    private class FormPanelModelImpl extends com.rameses.rcp.common.FormPanelModel {
+        
+        DBReportModel root = DBReportModel.this; 
+
+        public List<Map> getControlList() {  
+            return root.getFormControls(); 
+        } 
+    } 
     
-    private JasperPrint createReport() {
-        Connection conn = null;
-        try {
-            Class.forName(DBServiceDriver.class.getName());
-            if (mainReport == null) {
-                mainReport = ReportUtil.getJasperReport(getReportName());
-            }
-            
-            Map conf = new HashMap();
-            SubReport[] subReports = getSubReports();
-            
-            if (subReports != null) {
-                for (SubReport sr: subReports) {
-                    conf.put( sr.getName(), sr.getReport() );
-                }
-            }
-            Map params = getParameters();
-            if (params != null) conf.putAll(params);
-            
-            String appUrl = AppContext.getInstance().getEnv().get("app.host")+"";
-            String appContext = AppContext.getInstance().getEnv().get("app.context")+"";
-            String cluster = AppContext.getInstance().getEnv().get("app.cluster")+"";
-            String surl = "jdbc:rameses://"+ appUrl +"/"+ cluster + "/" + appContext;
-            conn = DriverManager.getConnection( surl );
-            
-            String parentName  =  "";
-            String rptName = getReportName();
-            if(rptName.indexOf("/")>0) {
-                parentName = rptName.substring(0, rptName.lastIndexOf("/"));
-            }
-            conf.put(JRParameter.REPORT_CLASS_LOADER, new CustomReportClassLoader(parentName));
-            return JasperFillManager.fillReport(mainReport, conf, conn );
-        } catch (RuntimeException re) {
-            throw re;
-        } catch (JRException ex) {
-            ex.printStackTrace();
-            throw new IllegalStateException(ex.getMessage(), ex);
-        } catch(Exception e) {
-            e.printStackTrace();
-            throw new IllegalStateException(e.getMessage(), e);
-        } finally {
-            try {conn.close();} catch(Exception ign){;}
+    private class ReportModelImpl extends ReportModel { 
+                
+        DBReportModel root = DBReportModel.this; 
+        JasperReport mainReport;
+        JasperPrint reportOutput;
+        
+        public Object getReportData() {
+            return null; 
         }
-    }
-    
-   
-    public String viewReport() {
-        reportOutput = createReport();
-        mode = "report";
-        return "report";
-    }
-    
-    public JasperPrint getReport() {
-        return reportOutput;
-    }
-    
-    /*
-    public List getReportActions() {
-        List list = new ArrayList();
-        list.add( new Action("_close", "Close", null));
-        return list;
-    }
-     */
-    
-    //this method is invoked by the back button
-    public Object back() {
-        mode = "initial";
-        return "default";
-    }
-    public Object close() {
-        mode = "initial";
-        return "_close";
-    }
-    
-    public String getMode() {
-        return mode;
+        public String getReportName() {
+            return root.getReportName(); 
+        }
+        public Map getParameters() {
+            return root.getQuery(); 
+        }
+        public SubReport[] getSubReports() {
+            return root.getSubReports(); 
+        }
+        public JasperPrint getReport() {
+            return reportOutput;
+        } 
+        
+        JasperPrint createReport() {
+            Connection conn = null;
+            try {
+                Class.forName(DBServiceDriver.class.getName());
+                
+                if (ReportUtil.isDeveloperMode()) { mainReport = null; }
+                
+                if (mainReport == null || root.isDynamic()) {
+                    mainReport = ReportUtil.getJasperReport( getReportName() );
+                }
+
+                Map conf = new HashMap();
+                SubReport[] subReports = getSubReports();
+                if (subReports != null) {
+                    for (SubReport sr: subReports) {
+                        conf.put( sr.getName(), sr.getReport() );
+                    }
+                }
+                Map params = getParameters();
+                if (params != null) conf.putAll(params);
+
+                String appUrl = AppContext.getInstance().getEnv().get("app.host")+"";
+                String appContext = root.getContext(); 
+                String cluster = root.getCluster();
+                String surl = "jdbc:rameses://"+ appUrl +"/"+ cluster + "/" + appContext;
+                conn = DriverManager.getConnection( surl );
+
+                String parentName  =  "";
+                String rptName = getReportName();
+                if(rptName.indexOf("/")>0) {
+                    parentName = rptName.substring(0, rptName.lastIndexOf("/"));
+                }
+                conf.put(JRParameter.REPORT_CLASS_LOADER, new CustomReportClassLoader(parentName));
+                reportOutput = JasperFillManager.fillReport(mainReport, conf, conn ); 
+                return reportOutput; 
+            } catch (RuntimeException re) {
+                throw re;
+            } catch (JRException ex) {
+                ex.printStackTrace();
+                throw new IllegalStateException(ex.getMessage(), ex);
+            } catch(Exception e) {
+                e.printStackTrace();
+                throw new IllegalStateException(e.getMessage(), e);
+            } finally {
+                try {conn.close();} catch(Exception ign){;}
+            }
+        }
+        
     }
 }
