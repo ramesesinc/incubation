9d9b56245f656c7a28cfe35b8f397be97c06a431
worgflores <worgflores@gmail.com>
2015-12-15 11:22:57 +0800

    anubis-cms: updated the js libs

--- anubis-cms2/anubis.lib/default/js/lib/rameses-session.js
+++ anubis-cms2/anubis.lib/default/js/lib/rameses-session.js
@@ -1,21 +1,53 @@
 var Socket = new function() {
 
-    function HttpSocket ( channel ) {
-        if (!channel) {
-            alert('channel must be provided!');
-            throw new Error('channel must be provided!');
-        }	
+	function Notifier( handlers, result ) {
+
+		var _self = this;
+		var _handlers = handlers;
+		var _result = result;
+
+		var notifyImpl = function( data ) { 
+			window.console.log( data );  
+			for( var n in _handlers ) {
+				try {
+					_handlers[n]( data );
+				} catch(e){;} 
+			} 
+		}
+
+		this.run = function() {
+			if ( _result instanceof Array ) {
+				for ( var i=0; i<_result.length; i++ ) {
+					notifyImpl( _result[i] ); 
+				}
+			} else {
+				notifyImpl( _result ); 
+			} 
+		} 
+	}
+
+    function HttpSocket ( channel ) { 
+        if (!channel) { 
+            alert('channel must be provided!'); 
+            throw new Error('channel must be provided!'); 
+        } 
 
         var self = this;
         var _channel = channel;
-        var _token =  Math.floor(Math.random()*1000000001);
+        var _token =  Math.floor(Math.random()*1000000001) + '-' + Math.floor(Math.random()*1000000001);
+        var _running = false;
 
         this.handlers = {};
         this.error;
 
+        this.getToken = function() {
+        	return _token; 
+        } 
+
         var poll = function() { 
-			try 
-			{
+        	if (_running == false) return; 
+
+			try {
 				$.ajax({ 
 					url: "/poll/"+_channel+"/"+_token, 
 					dataType: 'text',
@@ -27,52 +59,55 @@ var Socket = new function() {
 								try { 
 									result = $.parseJSON(o.responseText); 
 								} catch(e) {
-									if (window.console) 
-										window.console.log("ERROR parsing caused by " + e + " responseText->" + o.responseText); 
+									if (window.console) window.console.log("ERROR parsing caused by " + e + " responseText->" + o.responseText); 
 								} 
-							}        
-							if (result) { 
-								for( var n in self.handlers ) {
-									try {
-										self.handlers[n]( result );
-									}
-									catch(e){;}
-								} 
-							}
-							poll();
+							}  
+
+							if ( result ) { 
+								new Notifier( self.handlers, result ).run(); 
+							} 
+
+							setTimeout( poll, 1000 );
 						} 
 					} ,
 					error: function(o,textStatus,msg) {
 						if(textStatus=="timeout") {
 							poll();
-						}
-						else {
+						} else {
 							if( self.error ) {
 								self.error( o );
-							}    
-							else {
-								if(window.console) {
-									window.console.log( "Error in polling. " + o.error );
-								} 
+							} else {
+								if( window.console ) window.console.log( "Error in polling. " + o.error);
 								//reconnect after 30 seconds
-								window.console.log( "reconnecting" );   
 								setTimeout( poll, 30000 );
 							}    
 						}
 					}
 				});
 			} 
-			catch(e) {
+			catch(e) { 
 				if (window.console) window.console.log("[ERROR]_poll caused by " + e); 
 			} 
-        }
-        this.start = function(){
-            poll();
         } 
+        
+        this.start = function(){
+        	if (_running == true) {
+        		//do nothing 
+        	} else {
+	        	_running = true; 
+	            poll();
+        	}
+        } 
+
+        this.stop = function() {
+        	_running = false; 
+        }
     } 
 
     this.create = function( channel ) {
         var socket = new HttpSocket( channel );
         return socket;
-    }
+    } 
+
+
 } 
