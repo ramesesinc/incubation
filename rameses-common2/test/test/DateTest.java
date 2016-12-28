4be6b2b0dcbef18cd90adc24b24687a9e5c8ea39
Elmo Nazareno <elmonazareno@gmail.com>
2016-12-28 10:08:22 +0800

    Fixed holiday function

--- rameses-common2/test/test/DateTest.java
+++ rameses-common2/test/test/DateTest.java
@@ -7,8 +7,10 @@
 
 package test;
 
-import com.rameses.util.DateUtil;
+import com.rameses.functions.DateFunc;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import junit.framework.*;
 
 /**
@@ -28,10 +30,25 @@ public class DateTest extends TestCase {
     }
     
     // TODO add test methods here. The name must begin with 'test'. For example:
-    public void testHello() {
-        Date d1 = java.sql.Date.valueOf("2010-01-20");
-        Date d2 = DateUtil.add( d1, "1M" );
-        System.out.println(d2);
+    public void testFindNextWorkday() {
+        Date d1 = java.sql.Date.valueOf("2016-12-31");
+        List<Date> list = new ArrayList();
+        list.add( java.sql.Date.valueOf("2017-03-05"));
+        list.add( java.sql.Date.valueOf("2017-01-02"));
+        list.add( java.sql.Date.valueOf("2017-01-03"));
+        list.add( java.sql.Date.valueOf("2017-03-10"));
+        list.add( java.sql.Date.valueOf("2017-01-04"));
+        list.add( java.sql.Date.valueOf("2017-01-05"));
+        list.add( java.sql.Date.valueOf("2017-01-06"));
+        list.add( java.sql.Date.valueOf("2017-01-07"));
+        list.add( java.sql.Date.valueOf("2017-01-09"));
+        for( Date d: list ) {
+            System.out.println(d);
+        }
+        
+        Date d2 = DateFunc.getFindNextWorkDay(d1, list);
+        System.out.println("New date is " + d2);
     }
 
 }
+
