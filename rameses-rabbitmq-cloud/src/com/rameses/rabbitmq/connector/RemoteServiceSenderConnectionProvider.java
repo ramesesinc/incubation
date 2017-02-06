e77c62eb8a169adebb40b88122d6f5fc99af5428
worgflores <worgflores@gmail.com>
2017-02-06 17:52:33 +0800

    osiris2-client-ui: rameses-rabbitmq-cloud:

---
+++ rameses-rabbitmq-cloud/src/com/rameses/rabbitmq/connector/RemoteServiceSenderConnectionProvider.java
@@ -0,0 +1,146 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rameses.rabbitmq.connector;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+import com.rameses.osiris3.xconnection.MessageConnection;
+import com.rameses.osiris3.xconnection.XConnection;
+import com.rameses.osiris3.xconnection.XConnectionProvider;
+import com.rameses.service.ScriptServiceContext;
+import com.rameses.service.ServiceProxy;
+import com.rameses.util.Base64Cipher;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author wflores 
+ */
+public class RemoteServiceSenderConnectionProvider extends XConnectionProvider {
+
+    private final static String PROVIDER_NAME = "remote-service-sender";
+    private final static String DEFAULT_QUEUE_NAME = "default-queue";
+            
+    private ConnectionFactory factory; 
+    private Connection connection;
+    private Channel channel;
+    
+    private String queue;
+    private Sender sender; 
+    
+    public String getProviderName() {
+        return PROVIDER_NAME; 
+    }
+
+    public XConnection createConnection(String name, Map conf) { 
+        try { 
+            System.out.println("remote service connection create...");
+            RemoteServiceSenderConnection conn = new RemoteServiceSenderConnection(name, context, conf ); 
+            loadChannel( conf, conn ); 
+            return conn; 
+        } catch(RuntimeException re) { 
+            throw re; 
+        } catch(Exception e) { 
+            throw new RuntimeException(e.getMessage(), e); 
+        } 
+    }
+    
+    void loadChannel( Map conf, RemoteServiceSenderConnection conn ) { 
+        try { 
+            if ( factory == null ) { 
+                factory = new ConnectionFactory(); 
+                factory.setHost( (String) conf.get("host") ); 
+                factory.setUsername( (String) conf.get("user") ); 
+                factory.setPassword( (String) conf.get("pwd") ); 
+
+                int heartbeat = 60; 
+                try { 
+                    heartbeat = Integer.parseInt(conf.get("heartbeat").toString()); 
+                } catch(Throwable t) {;} 
+
+                factory.setRequestedHeartbeat( heartbeat ); 
+                factory.setAutomaticRecoveryEnabled(true); 
+            }  
+            
+            if ( connection == null ) { 
+                connection = factory.newConnection(); 
+            } 
+            
+            if ( channel == null ) { 
+                channel = connection.createChannel(); 
+                
+                queue = (String) conf.get("queue"); 
+                if ( queue != null ) {
+                    channel.queueDeclarePassive( queue ); 
+                }
+                
+                String channelgroup = (String) conf.get("channelgroup"); 
+                String exchange = (String) conf.get("exchange");
+                if ( exchange != null ) {
+                    channel.exchangeDeclare( exchange, "direct", true );
+                    //channel.queueBind( queue, exchange, channelgroup);
+                } 
+                conn.setChannel( channel );
+            } 
+        } catch (RuntimeException re) { 
+            throw re; 
+        } catch (Exception e) { 
+            throw new RuntimeException( e.getMessage(), e ); 
+        } 
+    } 
+    
+    // <editor-fold defaultstate="collapsed" desc=" Sender ">
+    
+    public class Sender {
+        
+        private String queue; 
+        private String exchange;
+        private String channelgroup;
+        
+        private Base64Cipher base64; 
+        
+        Sender( String queue, String exchange, String channelgroup ) {
+            this.queue = queue;
+            this.exchange = exchange;
+            this.channelgroup = channelgroup;
+            this.base64 = new Base64Cipher(); 
+        }
+        
+        public void send( Object message ) { 
+            send( base64.encode( message ).getBytes() );  
+        } 
+        
+        public void send( String message ) { 
+            if ( base64.isEncoded( message )) {
+                send( message.getBytes() ); 
+            } else {
+                send( base64.encode( message ).getBytes() );
+            }
+        } 
+        
+        public void send( byte[] message ) { 
+            try { 
+                if ( exchange == null) { 
+                    channel.basicPublish("", queue, null, message); 
+                } else { 
+                    channel.basicPublish( exchange, channelgroup, null, message);
+                } 
+            } catch (RuntimeException re) { 
+                throw re; 
+            } catch (Exception e) { 
+                throw new RuntimeException( e.getMessage(), e ); 
+            }                
+        }
+    }
+    
+    // </editor-fold> 
+    
+}
