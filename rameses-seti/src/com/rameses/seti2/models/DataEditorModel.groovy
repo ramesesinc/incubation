5aace995f8b1d045805c798951c46ad82519e76c
elmonazareno <elmonazareno@gmail.com>
2018-11-20 22:22:22 +0800

    added data editor model and change info service in seti-services

--- rameses-seti/src/com/rameses/seti2/models/DataEditorModel.groovy
+++ rameses-seti/src/com/rameses/seti2/models/DataEditorModel.groovy
@@ -10,36 +10,40 @@ import com.rameses.rcp.constant.*;
 import java.rmi.server.*;
 import com.rameses.util.*;
 
+/*******************************************************************************
+ * Limitation: There should only be 1 primary key
+ *******************************************************************************/
 public class DataEditorModel extends DynamicForm {
     
-    @Service("PersistenceService")
-    def persistenceSvc;
+    @Service("ChangeInfoService")
+    def changeInfoSvc;
     
+    @Service("SchemaService")
+    def schemaSvc;
+
     //entity is the original record
-    def entity;
-    
+    def primKey;
     def newValues;
     def oldValues;
     def remarks;
     def handler;
+    def _schema;
     boolean _inited_ = false;
     
     def message = "Fill in data to edit, specify remarks and click OK to commit changes";
     
     public String getSchemaName() {
+        def _schemaname = invoker.properties.schemaName;
+        if(_schemaname) return _schemaname;
         return workunit?.info?.workunit_properties?.schemaName;
+    } 
+    
+    public String getLogSchemaName() {
+        return workunit?.info?.workunit_properties?.logSchemaName;
     } 
     
     public String getTag() {
         return invoker.properties.tag;
-    }
-
-    void setFieldToEdit( String key ) {
-        if( entity == null ) throw new Exception("Error setValue. entity must not be null");
-        if( data == null ) throw new Exception("Error setValue. data must not be null");
-        
-        oldValues = entity.get(key);
-        data.put(key, entity.get(key) );
     }
     
     //This is if you want hghly generic editing.
@@ -49,39 +53,72 @@ public class DataEditorModel extends DynamicForm {
     void beforeInit() {;}
     void initData() {;}
     
-    void fetchData(def fields) {
-        throw new Exception("Please override fetchData( fields )");
-    }
+    
     void setDataForUpdate() {
         throw new Exception("Please override setDataForUpdate");
     }
     
+    public def getFormFields() {
+        return null;
+    }
+    
+    public def getEntity() {
+        return caller.entity;
+    }
+    
+    void buildFormFields() {
+        fields = [];
+        def fieldNames = invoker.properties.fields;
+        if(!fieldNames) return;
+        fieldNames.split(",").each { name->
+            fields << _schema.fields.find{it.name == name.trim()};
+        }
+    }
+    
     void init() {
         beforeInit();
-        if( !entity ) entity = caller.entity; 
         data = [:];
-        /*
-        def schema = persistenceSvc.getSchema( [name: schemaName] );
-        def primKeys = schema.fields.findAll{it.primary==true && it.source==schemaName}*.name;
-        primKeys.each {k->
-            data.put( k, entity.get(k));
+        _schema = schemaSvc.getSchema( [name: schemaName] );
+        primKey = _schema.fields.find{it.primary==true && it.source==schemaName}.name;
+        fields = getFormFields();
+        if(fields == null) buildFormFields();
+        fields.each {
+            data.put(it.name, entity.get(it.name));
         }
-        data.put(key, entity.get( key ) );
-        fields = [];
-        fields.addAll( getDataFields() );
-        */
-        fields = [];
-        fetchData(fields);
         buildFormInfos();
+        oldValues = MapBeanUtils.copy(data);
         _inited_ = true;
+    }
+    
+    void validateEntry() {
+        //do nothing. Throw exception if theres an error.
     }
     
     def doOk() {
         if(!_inited_) throw new Exception("Please invoke init action on thos workunit");
-        setDataForUpdate();
-        data._tag = tag;
-        if(!data._schemaname ) data._schemaname = schemaName;
-        persistenceSvc.update( data );
+        if( oldValues == data )
+            throw new Exception("No change has been made");
+        
+        validateEntry();
+        def u = [:];
+        u.putAll( data );
+        
+        def e = [:];
+        e._tag = tag;
+        e._schemaname = schemaName;
+        e.putAll( u );
+        e.put( primKey, entity.get( primKey ) );
+        
+        //build the log data
+        def log = [_schemaname: getLogSchemaName() ];
+        log.reftype = schemaName;
+        log.refid = entity.get( primKey );
+        log.remarks = remarks;
+        log.oldvalue = oldValues;
+        log.newvalue = u;
+        log.action = "update";
+        e._loginfo = log; 
+        changeInfoSvc.save( e );
         if(handler) handler();
         return "_close";
     }
