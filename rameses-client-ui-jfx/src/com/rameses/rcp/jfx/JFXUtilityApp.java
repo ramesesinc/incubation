fac0be7d01d68da22736b5ed3e14b13e7d7a8a24
worgflores <worgflores@gmail.com>
2019-10-25 16:08:27 +0800

    rameses-client-ui-jfx: updated

--- rameses-client-ui-jfx/src/com/rameses/rcp/jfx/JFXUtilityApp.java
+++ rameses-client-ui-jfx/src/com/rameses/rcp/jfx/JFXUtilityApp.java
@@ -4,6 +4,7 @@
  */
 package com.rameses.rcp.jfx;
 
+import java.io.InputStream;
 import javafx.application.Application;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
@@ -16,15 +17,12 @@ import javafx.stage.StageStyle;
 public class JFXUtilityApp extends Application {
 
     public void start(Stage stage) throws Exception {
-        System.out.println("start( Stage )...");
         stage.setWidth(1);
         stage.setHeight(1);
         stage.setX(-200);
         stage.setY(-200);
         stage.initStyle(StageStyle.UTILITY); 
-        System.out.println("before show stage...");
         stage.show(); 
-        System.out.println("after show stage...");
     } 
     
     public static void main(String[] args) { 
@@ -34,9 +32,10 @@ public class JFXUtilityApp extends Application {
         };
         for ( String str : fonts ) { 
             try { 
-                Font.loadFont(JFXUtilityApp.class.getClassLoader().getResourceAsStream( str ), 11); 
+                InputStream inp = JFXUtilityApp.class.getClassLoader().getResourceAsStream( str ); 
+                if ( inp != null ) Font.loadFont(inp, 11); 
             } catch(Throwable t) {
-                t.printStackTrace();
+                //t.printStackTrace();
             }
         }
         
